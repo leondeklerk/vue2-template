1b02dc5ae75c2810248330e8a8d9f80be2653516
Leon de Klerk <developer@leondeklerk.com>
2022-07-25 17:32:08 +0200

    Add template
    Signed-off-by: Leon de Klerk <developer@leondeklerk.com>

---
+++ README.md
@@ -0,0 +1,13 @@
+# Vue 2 Template
+A Vue 2 template repository.
+
+Configured with:
+
+- Renovate
+- Eslint (Prettier)
+- Sass
+- Vue class components
+- TypeScript
+- Docker build pipeline
+- FontAwesome
+- Bootstrap
---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="app">
+        <Screen />
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Screen from "./components/Screen.vue";
+
+@Component({
+    components: {
+        Screen
+    }
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 0.5em;
+    height: 100%;
+    margin-left: 0.5em;
+    margin-right: 0.5em;
+}
+</style>
