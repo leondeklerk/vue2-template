de562a53201fd8c0265ca2a825eaadf10387ab40
Leon de Klerk <developer@leondeklerk.com>
2023-03-05 16:43:04 +0100

    Update fontawesome setup
    Signed-off-by: Leon de Klerk <developer@leondeklerk.com>

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import "bootstrap/dist/css/bootstrap.css";
-
-/* import the fontawesome core */
-import { library } from "@fortawesome/fontawesome-svg-core";
-
-/* import font awesome icon component */
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-
-/* import specific icons */
-import {} from "@fortawesome/free-solid-svg-icons";
-
-/* add icons to the library */
-library.add();
-
-/* add font awesome icon component */
-Vue.component("font-awesome-icon", FontAwesomeIcon);
+import "@fortawesome/fontawesome-free/css/fontawesome.css";
+import "@fortawesome/fontawesome-free/css/brands.css";
+import "@fortawesome/fontawesome-free/css/solid.css";
 
 Vue.config.productionTip = false;
 
